05bab19a2d6b61d85606f28c2f5061bdbb5ba5c7
Rafał Głowacki <107753644+MrRafalGlowacki@users.noreply.github.com>
2023-03-13 19:20:22 +0100

    fetch() added

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,16 +1,16 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
 
-axios.defaults.baseURL = 'https://6408ba178ee73db92e49b1f2.mockapi.io';
+const URL = 'https://6408ba178ee73db92e49b1f2.mockapi.io/contacts';
 
 export const fetchContactsAction = createAsyncThunk(
   'contacts/fetchAll',
   async (_, thunkAPI) => {
     try {
-      const response = await axios.get('/contacts');
-      return response.data;
+      const response = await fetch(URL);
+      const data = await response.json();
+      return data;
     } catch (e) {
-      thunkAPI.rejectWithValue(e.massege);
+      thunkAPI.rejectWithValue(e.message);
     }
   }
 );
@@ -19,21 +19,32 @@ export const addContactAction = createAsyncThunk(
   'contacts/addContact',
   async ({ name, number }, thunkAPI) => {
     try {
-      const response = await axios.post('/contacts', { name, number });
-      return response.data;
+      const response = await fetch(URL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name, number }),
+      });
+      const data = await response.json();
+      return data;
     } catch (e) {
-      return thunkAPI.rejectWithValue(e.massege);
+      return thunkAPI.rejectWithValue(e.message);
     }
   }
 );
+
 export const deleteContactAction = createAsyncThunk(
   'contacts/deleteContact',
   async (id, thunkAPI) => {
     try {
-      const response = await axios.delete(`/contacts/${id}`);
-      return response.data;
+      const response = await fetch(URL + '/' + id, {
+        method: 'DELETE',
+      });
+      const data = await response.json();
+      return data;
     } catch (e) {
-      return thunkAPI.rejectWithValue(e.massege);
+      return thunkAPI.rejectWithValue(e.message);
     }
   }
 );
